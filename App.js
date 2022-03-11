2775fcd717c4b753f9ac62a775e6e1f32014426e
romainpltr <poultier.romain@gmail.com>
2022-03-11 14:25:57 +0100

    refactor: app js

--- App.js
+++ App.js
@@ -1,46 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, FlatList, View } from 'react-native';
-import Button from './components/atoms/button';
-import { getPokemon } from './api/getPokemons';
-import { PokemonCard } from './components/molecules/PokemonCard';
-import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import Navigation from './navigation/Navigation';
+import PokemonList from './components/organisms/PokemonList';
 
 export default function App() {
-  const [listPokemons, setListPokemons] = useState({});
-  async function onLoad() {
-    const pokemons = await getPokemon()
-    setListPokemons(pokemons);
-  };
-
-  useEffect(() => {
-    onLoad();
-    console.log(listPokemons);
-  }, []);
-
-  const onClick = (color) => {
-    console.log(color)
-  }
-
-  const renderItem = ({ item }) => (
-    <PokemonCard name={item.name} link={item.url} />
-  )
 
   return (
-    <View style={styles.container}>
-      <FlatList data={listPokemons} renderItem={renderItem} />
-      <Button btnTitle="Oui oui je suis dans l'espace, dans l'espace" onClick={onClick} color="blue"/>
-      <Button btnTitle="EWWWWW EWWWWW EWWWW Et non je ne suis pas yann solo, ceci est une descente de police" onClick={onClick}  color="red"/>
-      <Button btnTitle="Je vous demande d'enlever vos chaussettes" onClick={onClick} color="green"/>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

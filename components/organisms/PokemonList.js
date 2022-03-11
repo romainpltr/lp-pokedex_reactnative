78b935e91ada0d199104993ed4bc47d06ba50013
romainpltr <poultier.romain@gmail.com>
2022-03-11 14:20:24 +0100

    feat: added PokemonList organisms

---
+++ components/organisms/PokemonList.js
@@ -0,0 +1,75 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, FlatList, View, ScrollView } from 'react-native';
+import { useEffect, useState, useCallback } from 'react';
+
+import { getPokemon } from '../../api/getPokemons';
+import PokemonCard from '../molecules/PokemonCard';
+
+function PokemonsList() {
+  const [listPokemons, setListPokemons] = useState([]);
+  const [nextPage, setNextPage] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    if(isMounted){
+      !isLoading && loadPokemon(nextPage);
+    }
+    return () => { 
+      isMounted = false;
+    }
+  }, []);
+
+  const loadPokemon = async (url) => {
+     await getPokemon(url).then((datas) => { 
+      setListPokemons([...listPokemons, ...datas.results]);
+      setNextPage(datas.next)
+      setIsLoading(false);
+    })
+  }
+
+  const renderItem = useCallback(({ item }) => (
+    <PokemonCard style={styles.PokemonCard} name={item.name} link={item.url} />
+  ), [])
+
+  const keyExtractor = useCallback((item) => item.name);
+  const infiniteScrollLoad = () => { loadPokemon(nextPage) };
+
+  return (
+    <View style={styles.pokemonGrid}>
+        <FlatList
+          data={listPokemons} 
+          renderItem={renderItem} 
+          keyExtractor={keyExtractor}
+          onEndReached={infiniteScrollLoad}
+          onEndReachedThreshold={0.5}
+          numColumns={3}
+          windowSize={18}
+          maxToRenderPerBatch={18}
+          removeClippedSubviews={true}
+          updateCellsBatchingPeriod={100}
+          ListFooterComponent={() =>
+            isLoading && (
+              <View style={styles.loader}>
+                <ActivityIndicator size={"large"} color={"blue"} />
+              </View>
+            )
+          }
+
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  PokemonCard: {
+
+  },
+  pokemonGrid: {
+    display: 'flex',
+    backgroundColor: '#e06666',
+    marginTop: 20,
+  }
+});
+
+export default PokemonsList;
